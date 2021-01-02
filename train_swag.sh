1e2249f45e729509d49f9e00645561c1dd4184ea
MilesCranmer <miles.cranmer@gmail.com>
2021-01-02 06:04:26 -0500

    Better filenames

--- train_swag.sh
+++ train_swag.sh
@@ -1,4 +1,4 @@
 for seed in `seq 0 29`; do
-    python swag_part1.py --total_steps 300000 --swa_steps 50000 --version 51 --no_mmr --no_nan --no_eplusminus --seed $seed
-    python swag_part2.py --total_steps 300000 --swa_steps 50000 --version 51 --no_mmr --no_nan --no_eplusminus --seed $seed
+    python find_minima.py --total_steps 300000 --swa_steps 50000 --version 51 --no_mmr --no_nan --no_eplusminus --seed $seed
+    python run_swag.py --total_steps 300000 --swa_steps 50000 --version 51 --no_mmr --no_nan --no_eplusminus --seed $seed
 done
