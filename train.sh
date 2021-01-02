fac2e02ccc58df10479ba5bff585896842b75d3b
MilesCranmer <miles.cranmer@gmail.com>
2021-01-02 07:47:30 -0500

    Fix model version

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 for seed in `seq 0 29`; do
-    python find_minima.py --total_steps 300000 --swa_steps 50000 --version 51 --no_mmr --no_nan --no_eplusminus --seed $seed
-    python run_swag.py --total_steps 300000 --swa_steps 50000 --version 51 --no_mmr --no_nan --no_eplusminus --seed $seed
+    python find_minima.py --total_steps 300000 --swa_steps 50000 --version 50 --angles --no_mmr --no_nan --no_eplusminus --seed $seed
+    python run_swag.py --total_steps 300000 --swa_steps 50000 --version 50 --angles --no_mmr --no_nan --no_eplusminus --seed $seed
 done
