f6da5688112add4f21f87df69118d85dd7df66a0
MilesCranmer <miles.cranmer@gmail.com>
2021-01-04 14:44:27 -0500

    Make training script exit upon any error

--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+set -e
 for seed in `seq 0 29`; do
-    python find_minima.py --total_steps 300000 --swa_steps 50000 --version 50 --angles --no_mmr --no_nan --no_eplusminus --seed $seed
-    python run_swag.py --total_steps 300000 --swa_steps 50000 --version 50 --angles --no_mmr --no_nan --no_eplusminus --seed $seed
+    python find_minima.py --total_steps 300000 --swa_steps 50000 --version 53 --angles --no_mmr --no_nan --no_eplusminus --seed $seed
+    python run_swag.py --total_steps 300000 --swa_steps 50000 --version 53 --angles --no_mmr --no_nan --no_eplusminus --seed $seed
 done
